b462fdbe08876d61c629377c4edef998165cb139
John <jbotonakis@carroll.edu>
2025-04-02 16:56:06 -0600

    renamed main file

--- DiningPhilosophers.java
+++ DiningPhilosophers.java
@@ -2,7 +2,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class Main{
+public class DiningPhilosophers{
     public static void main(String[] args) throws InterruptedException {
 
         int numPhilosophers = 5;
